10e1c2c24cd06d2e1a93f012b128dce664b1fe8b
Salehen Shovon Rahman <salehen.rahman@gmail.com>
2013-08-23 15:49:41 -0700

    Tested getDeviceData.

--- test.js
+++ test.js
@@ -3,7 +3,9 @@
 
 'use strict';
 
-var expect      = require('expect.js'),
+var _           = require('lodash'),
+    expect      = require('expect.js'),
+    assert      = require('assert'),
     bcpmHelpers = require('./utilities/lib/bcpm-helpers');
 
 describe('bcpm_helpers', function () {
@@ -32,6 +34,64 @@ describe('bcpm_helpers', function () {
             }
           ];
       expect(bcpmHelpers.getBcpmData(data)).to.eql(expected);
+    });
+  });
+  describe('getDeviceData', function () {
+    it('should group bcpm by their device number', function () {
+      var data = [
+            {
+              Name: 'bcpm_10_kw'
+            },
+            {
+              Name: 'bcpm_11_kwh'
+            },
+            {
+              Name: 'bcpm_10_kwh'
+            },
+            {
+              Name: 'bcpm_11_kw'
+            },
+            {
+              Name: 'bcpm_12_kwh'
+            },
+            {
+              Name: 'bcpm_12_kw'
+            }
+          ],
+          expected = {
+            '10': [
+              {
+                Name: 'bcpm_10_kw'
+              },
+              {
+                Name: 'bcpm_10_kwh'
+              }
+            ],
+            '11': [
+              {
+                Name: 'bcpm_11_kw'
+              },
+              {
+                Name: 'bcpm_11_kwh'
+              }
+            ],
+            '12': [
+              {
+                Name: 'bcpm_12_kw'
+              },
+              {
+                Name: 'bcpm_12_kwh'
+              }
+            ]
+          },
+          result = bcpmHelpers.getDeviceData(data);
+
+      _.keys(expected).forEach(function (el) {
+        assert(!!result[el]);
+        expect(result[el].sort(function (a, b) {
+          return a.Name > b.Name ? 1 : (a.Name < b.Name ? -1 : 0);
+        })).to.eql(expected[el]);
+      });
     });
   });
 });
--- utilities/lib/bcpm-helpers.js
+++ utilities/lib/bcpm-helpers.js
@@ -2,8 +2,16 @@
 
 'use strict';
 
+var _ = require('lodash');
+
 module.exports.getBcpmData = function (westHouseData) {
   return westHouseData.filter(function (el) {
     return (/^bcpm_([0-9]+)_kwh?$/).test(el.Name);
+  });
+};
+
+module.exports.getDeviceData = function (bcpmData) {
+  return _.groupBy(bcpmData, function (el) {
+    return el.Name.match(/[0-9]+/)[0];
   });
 };
