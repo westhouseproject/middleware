b4668f825b6167d27a409deac8d91758d1773ea1
Salehen Shovon Rahman <salehen.rahman@gmail.com>
2013-08-25 03:23:53 -0700

    This works now.

--- app.js
+++ app.js
@@ -22,7 +22,17 @@ app.get('/devices', function (req, res) {
 });
 
 app.get('/consumptions', function (req, res) {
-  res.json(501, { error: 'Not yet implemented.' });
+  utils.downloadMControlData(function (err, body) {
+    var devices, consumptions;
+    if (err) return res.json(500, { error: err.message });
+    try {
+      devices = JSON.parse(body);
+      consumptions = utils.getConsumptionData(devices);
+    } catch (e) {
+      return res.json(500, { error: e.message });
+    }
+    res.json(consumptions);
+  });
 });
 
 app.listen(3000);
