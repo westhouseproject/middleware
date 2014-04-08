73e43bbf6ee734ae462a90a699bacd82929d9ba2
Salehen Shovon Rahman <salehen.rahman@gmail.com>
2014-04-07 20:44:35 -0700

    Forgot to return.

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var connection = mysql.createConnection(settings.database);
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET');
+  res.next();
 });
 app.use(express.json());
 app.use(express.methodOverride());
